005879cf92c055d3ba4c655593e2930bbb4df2ad
Lionel B <lionel1232@gmail.com>
2019-06-30 21:47:34 +1000

    setup Spring MVC dispatcher servlet - add `spring-webmvc` as a dependency in `pom.xml` - add `javax.servlet-api` as a dependency in `pom.xml` - add `WebConfig.java` to specify a default config for the web application - add `WellAppInitializer.java` to register and configure the servlet

---
+++ src/main/java/lionel/learnspring/config/WebConfig.java
@@ -0,0 +1,11 @@
+package lionel.learnspring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "lionel.learnspring")
+public class WebConfig  {
+}
