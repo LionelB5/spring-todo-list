fb99cc90e781778b85876dd1edd6912332cf0eea
Lionel B <lionel1232@gmail.com>
2019-07-14 21:11:21 +1000

    modify `welcome` controller method to take two query parameters

--- src/main/java/lionel/learnspring/controller/DemoController.java
+++ src/main/java/lionel/learnspring/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -26,10 +27,11 @@ public class DemoController {
         return "hello";
     }
 
-    // http://localhost:8080/todo-list/welcome
+    // http://localhost:8080/todo-list/welcome?user=Lionel&age=24
     @GetMapping("welcome")
-    public String welcome(Model model) {
-        model.addAttribute("helloMessage", demoService.getHelloMessage("Lionel"));
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("model= {}", model);
         // prefix + name + suffix
         // /WEB-INF/view/welcome.jsp
--- src/main/webapp/WEB-INF/view/welcome.jsp
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -4,6 +4,7 @@
 </head>
 <body>
     <h1>${helloMessage}</h1>
+    <h2>age parameter is ${age}</h2>
     <h2>${welcomeMessage}</h2>
 </body>
 </html>
