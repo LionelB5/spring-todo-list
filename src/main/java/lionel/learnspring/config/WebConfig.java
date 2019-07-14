7a44e23c97df5e9618747564dae660db4ac0cd6f
Lionel B <lionel1232@gmail.com>
2019-07-14 18:40:36 +1000

    set up a basic example of usage of the ViewResolver - modified config to add ViewResolver bean - created new controller method to return name of new JSP view - created basic welcome.jsp view

--- src/main/java/lionel/learnspring/config/WebConfig.java
+++ src/main/java/lionel/learnspring/config/WebConfig.java
@@ -1,11 +1,26 @@
 package lionel.learnspring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "lionel.learnspring")
 public class WebConfig  {
+
+    public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
+    public static final String RESOLVER_SUFFIX = ".jsp";
+
+    @Bean
+    public ViewResolver viewResolver() {
+        UrlBasedViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix(RESOLVER_PREFIX);
+        viewResolver.setSuffix(RESOLVER_SUFFIX);
+        return viewResolver;
+    }
 }
--- src/main/java/lionel/learnspring/controller/HelloWorldController.java
+++ src/main/java/lionel/learnspring/controller/HelloWorldController.java
@@ -7,9 +7,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
+    // http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    // http://localhost:8080/todo-list/welcome
+    // prefix + name + suffix
+    // /WEB-INF/view/welcome.jsp
+    @GetMapping("welcome")
+    public String welcome() {
+        return "welcome";
     }
 }
