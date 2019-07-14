5fa1cf84213f0b3eb43664b83bfc6fc1a12710dc
Lionel B <lionel1232@gmail.com>
2019-07-14 20:18:36 +1000

    update the controller to add some attributes to the model

--- src/main/java/lionel/learnspring/controller/HelloWorldController.java
+++ src/main/java/lionel/learnspring/controller/HelloWorldController.java
@@ -1,9 +1,13 @@
 package lionel.learnspring.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class HelloWorldController {
 
@@ -15,10 +19,18 @@ public class HelloWorldController {
     }
 
     // http://localhost:8080/todo-list/welcome
-    // prefix + name + suffix
-    // /WEB-INF/view/welcome.jsp
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("user", "Tim");
+        log.info("model= {}", model);
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
         return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo application.";
     }
 }
